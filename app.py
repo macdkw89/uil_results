ce041f897ae2cd0ea6b049d6fb3aa40150002a4b
Kevin MacDonald <macdkw@gmail.com>
2022-11-05 15:56:18 -0500

    update

--- app.py
+++ app.py
@@ -1,19 +1,85 @@
-from dash import Dash, html, dcc, Input, Output, State, dash_table
-import plotly.express as px
-import pandas as pd
+from dash import Dash, html, dcc, dash_table, Input, Output, State
 from explorer import df
 
 app = Dash(__name__)
-# add a header
 
-app.layout = html.Div(
-    [
 
-dash_table.DataTable(df.to_dict('records'), [
-    {"name": i, "id": i} for i in df.columns])
+app.layout = html.Div(children=[
+    html.H1(children='UIL Results Explorer'),
+    html.Div(style={"width": "50%"}, children=[
+        html.Label(children='Search by School name:'),
+        dcc.Input(id='school-input', value='', type='text'),
+        html.Br(),
+        html.Label(children='Filter by conference:'),
+        dcc.Dropdown(
+            id='conference-dropdown',
+            options=[{'label': i, 'value': i}
+                     for i in df['Conference'].unique()],
+            value='Conference',
+            multi=True
+        ),
+
+        dcc.Dropdown(
+            id='classification-dropdown',
+            options=[{'label': i, 'value': i}
+                     for i in df['Classification'].sort_values().unique()],
+            value='Classification',
+            multi=True
+        ),
+
+        dcc.Dropdown(
+            id='year-dropdown',
+            options=[{'label': i, 'value': i} for i in df['Year'].sort_values().unique()],
+            value='Year',
+            multi=True
+        ),
+    ]),
+    dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict('records'),
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'rgb(230, 230, 230)',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            }
+        ],
+    )
 ])
 
+
+@ app.callback(
+
+    Output('table', 'data'),
+    [
+        Input('school-input', 'value'),
+        Input('conference-dropdown', 'value'),
+        Input('classification-dropdown', 'value'),
+        Input('year-dropdown', 'value')
+    ])
+def update_table(school, conference, classification, year):
+    dff = df
+    if school != 'School':
+        dff = dff[dff['School'].str.contains(school)]
+    if conference != 'Conference':
+        dff = dff[dff['Conference'] == conference]
+    if classification != 'Classification':
+        dff = dff[dff['Classification'] == classification]
+    if year != 'Year':
+        dff = dff[dff['Year'] == year]
+    return dff.to_dict('records')
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+'''
+dash_table.DataTable(df.to_dict('records'), [
+    {"name": i, "id": i} for i in df.columns])
+])'''
--- explorer.py
+++ explorer.py
@@ -3,16 +3,16 @@ import pandas as pd
 # read in data
 df = pd.read_csv('csv_files/full_output copy.csv', index_col=0, low_memory=False)
 
+# drop columns
+
+
+df = df.drop(columns=['City', 'Stage Final', 'SR Final', 'Award', 'Date', 'Stage Average', 'cj1', 'cj2', 'cj3', 'srj1', 'srj2', 'srj3', 'Region'])
+
 df['School'] = df['School'].astype(str)
-df['City'] = df['City'].astype(str)
 df['Directors'] = df['Directors'].astype(str)
 df['Conference'] = df['Conference'].astype(str)
 df['Classification'] = df['Classification'].astype(str)
 df['Year'] = df['Year'].astype(int)
-df['Stage Final'] = df['Stage Final'].astype(float)
-
-# convert date column to datetime
-df['Date'] = pd.to_datetime(df['Date'])
 
 # find columns with mixed data types
 weird_cols = []
